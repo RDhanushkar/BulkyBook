bb1b2b54c764246242537acaaf3509a7ddd59b34
Ravindran Dhanushkar <84069209+RDhanushkar@users.noreply.github.com>
2023-03-12 18:58:56 +0530

    Solve Invalid Object error.(The table was not create then I use EntityFrameworkCore\Add-migration AddCoverTypeToDb. After than create CoverType Controller and Action method and create view for crud. And add Client side and Server Side Validations

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//Cover Type add migration;
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
 
