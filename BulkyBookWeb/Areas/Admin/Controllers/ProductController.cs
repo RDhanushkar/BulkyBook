4d75dd9069e9b68e2050b94a14c9594cd442124b
Ravindran Dhanushkar <84069209+RDhanushkar@users.noreply.github.com>
2023-03-15 23:07:18 +0530

    add ViewModel and Re-arraged the code

--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using BulkyBook.DataAccess;
 using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using NuGet.Packaging.Signing;
@@ -26,35 +27,35 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
         
         public IActionResult Upsert(int? id)
         {
-            Product product = new();
-            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(
-                u=> new SelectListItem
+            ProductVM productVM = new()
+            {
+                Product = new(),
+                CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
                 {
-                    Text= u.Name,
-                    Value=u.Id.ToString(),
-                }
-                );
-            IEnumerable<SelectListItem> CoverTypeList = _unitOfWork.CoverType.GetAll().Select(
-                u => new SelectListItem
+                    Text = i.Name,
+                    Value = i.Id.ToString(),
+                }),
+
+                CoverTypeList = _unitOfWork.CoverType.GetAll().Select(i => new SelectListItem
                 {
-                    Text = u.Name,
-                    Value = u.Id.ToString(),
-                }
-                );
+                    Text = i.Name,
+                    Value = i.Id.ToString(),
+                }),
+            };
 
             if (id == null || id == 0)
             {
                 //Create Product
-                ViewBag.CategoryList = CategoryList;
-                ViewData["CoverTypeList"] = CoverTypeList;
-                return View(product);
+                //ViewBag.CategoryList = CategoryList;
+                //ViewData["CoverTypeList"] = CoverTypeList;
+                return View(productVM);
             }
             else
             {
                 //update product
             }
 
-            return View(product);
+            return View(productVM);
         }
 
         //POST Method
