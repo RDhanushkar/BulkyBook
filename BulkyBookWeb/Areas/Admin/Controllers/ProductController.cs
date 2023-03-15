fdf16ab8f871bbbafa5df7db21e986cf75d56852
Ravindran Dhanushkar <84069209+RDhanushkar@users.noreply.github.com>
2023-03-15 21:59:17 +0530

    Display the Ctegory Name and CoverType in  Product Form by ViewBag and ViewData methods

--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using BulkyBook.DataAccess;
 using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using NuGet.Packaging.Signing;
 
 
@@ -26,9 +27,26 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
         public IActionResult Upsert(int? id)
         {
             Product product = new();
+            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(
+                u=> new SelectListItem
+                {
+                    Text= u.Name,
+                    Value=u.Id.ToString(),
+                }
+                );
+            IEnumerable<SelectListItem> CoverTypeList = _unitOfWork.CoverType.GetAll().Select(
+                u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString(),
+                }
+                );
+
             if (id == null || id == 0)
             {
                 //Create Product
+                ViewBag.CategoryList = CategoryList;
+                ViewData["CoverTypeList"] = CoverTypeList;
                 return View(product);
             }
             else
