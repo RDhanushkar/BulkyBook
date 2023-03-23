6c68952fa6cd8a0b568ba09a32578690791c25ff
Ravindran Dhanushkar <84069209+RDhanushkar@users.noreply.github.com>
2023-03-23 23:28:52 +0530

    Do image update and delete in Upsert Form. Change the Description.

--- BulkyBookWeb/wwwroot/js/products.js
+++ BulkyBookWeb/wwwroot/js/products.js
@@ -27,7 +27,8 @@ function loadDataTable() {
                                 <i class="bi bi-pencil-square"></i>Edit
                             </a>
 
-                            <a class="btn btn-danger mx-2" >
+                            <a onClick=Delete('/Admin/Product/Delete/${data}')
+                            class="btn btn-danger mx-2" >
                                 <i class="bi bi-trash-fill"></i>Delete
                             </a>
                       </div>
@@ -38,4 +39,34 @@ function loadDataTable() {
             }
         ]
     });
+}
+
+function Delete(url) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax
+                ({
+                    url: url,
+                    type: 'DELETE',
+                    success: function (data)
+                    {
+                        if (data.success) {
+                            dataTable.ajax.reload();
+                            toastr.success(data.message);
+                        }
+                        else {
+                            toastr.error(data.message );
+                        }
+                    }
+                })
+        }
+    })
 }
