fc0ddd4c8cd56cb986a5729cf3105db0065ba107
Ravindran Dhanushkar <84069209+RDhanushkar@users.noreply.github.com>
2023-04-15 00:41:23 +0530

    Create Order Management for Admin.

---
+++ BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		public OrderController(IUnitOfWork unitOfWork) 
+		{
+			_unitOfWork = unitOfWork;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+		#region API CALLS
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			IEnumerable<OrderHeader> orderHeader;
+			orderHeader = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+			return Json(new { data = orderHeader });
+		}
+		#endregion
+	}
+}
---
+++ BulkyBookWeb/wwwroot/js/order.js
@@ -0,0 +1,39 @@
+﻿var dataTable;
+
+/*let table = new DataTable('#myTable');*/
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').dataTable({
+        "ajax": {
+            "url": "/Admin/Order/GetAll"
+        },
+        "columns": [
+            { "data": "id", "width": "5%" },
+            { "data": "name", "width": "25%" },
+            { "data": "phoneNumber", "width": "15%" },
+            { "data": "applicationUser.email", "width": "15%" },
+            { "data": "orderStatus", "width":"15%"},
+            { "data": "orderTotal", "width":"10%"},
+            {
+                "data": "id",
+
+                "render": function (data) {
+                    return `
+                        <div class="w-75 btn-group" role="group">
+                            <a href="/Admin/Order/Details?orderId=${data}"
+                            class="btn btn-primary mx-2">
+                                <i class="bi bi-pencil-square"></i>
+                            </a> 
+                      </div>
+                    `
+                },
+
+                "width": "5%"
+            }
+        ]
+    });
+}
+
